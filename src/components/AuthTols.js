b1616fa51c0dc0be3cd5116fa2717d0f9f0d5ae0
unknown <hagmir6@gamil.com>
2022-07-22 22:56:21 +0200

    first commit

--- src/components/AuthTols.js
+++ src/components/AuthTols.js
@@ -8,7 +8,7 @@ function Auth() {
     const { logoutUser, user } = useContext(AuthContext);
     const {t} = useTranslation()
     return (
-        <div className="dropdown">
+        <div className="dropdown d-lg-block d-none bg-white">
             <img src={user.avatar} width="40px" height={40}  className="cover border rounded-pill pointer" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" />
             <ul className='dropdown-menu dropdown-menu-end animate__animated animate__flipInX animate__faster' id="avatar-menu" aria-labelledby="dropdownMenuButton1">
                 <li><Link to={user.username} className="dropdown-item">{t('Profile')}</Link></li>
@@ -23,7 +23,7 @@ function NotAuth() {
     const {t} = useTranslation()
 
     return (
-        <div>
+        <div className="d-none d-lg-block">
             <Link to='/accounts/login' className="btn-primary btn rounded-pill mx-2">{t("Log in")} </Link>
             <Link to='/accounts/register' className="btn-defaul border btn-outline-defaul btn rounded-pill mx-2">{t("Register")} </Link>
         </div>
